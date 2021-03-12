108ed454e35753183993c4eaecc64162a96de7c6
szymon <szymon.piotr.duda@gmail.com>
2021-03-12 01:28:25 +0100

    Updates

---
+++ precache-manifest.a192eea70af3561b99d103ae4301b753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd532d245a68c61e36f7ba86fb358506",
+    "url": "/movieser/index.html"
+  },
+  {
+    "revision": "1d7a0b0512011f1728af",
+    "url": "/movieser/static/js/2.e5fd015d.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/movieser/static/js/2.e5fd015d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd345f529934a2b1385",
+    "url": "/movieser/static/js/main.b346b515.chunk.js"
+  },
+  {
+    "revision": "a3060a8bb657a8b305db",
+    "url": "/movieser/static/js/runtime-main.376ebd93.js"
+  },
+  {
+    "revision": "306d39c6116355b760fd5928eba87c5f",
+    "url": "/movieser/static/media/search.306d39c6.svg"
+  }
+]);
